133327f344ee7db46b0acb7d8d86f1e1acd9730b
miaharris06 <mia.harris2@my.jcu.edu.au>
2022-09-24 13:59:05 +1000

    Prints results or stars based on score

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,16 +3,29 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+MENU = "\n(R)esult \n(S)tars \n(Q)uit"
+
 
 def main():
-    score = float(input("Enter score: "))
-    print(determine_result(score))
+    score = int(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Enter score: "))
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "R":
+            print(determine_result(score))
+        elif choice == "S":
+            for i in range(score):
+                print("*", end="")
+        print(MENU)
+        choice = input(">>> ").upper()
+
 
 
 def determine_result(score):
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score >= 90:
+    if score >= 90:
         return "Excellent"
     elif score >= 50:
         return "Passable"
