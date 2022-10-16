dd5254341e7fb499447c3329ec7ac35e12928809
miaharris06 <mia.harris2@my.jcu.edu.au>
2022-10-16 20:55:00 +1000

    Complete prac 5 word occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,8 +8,8 @@ text = input("Text: ")
 words = text.split(" ")
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
-    max_length = max(len(words) for word in list(word_to_count.keys()))
 words = list(word_to_count.keys())
 words.sort()
+max_length = max(len(words) for word in words)
 for word in words:
     print(f"{word:{max_length}} : {word_to_count[word]} ")
